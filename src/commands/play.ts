870c60c073ad0aa6d24743226665704f3d0ccfac
Jovem-Blood <thiagopeixot13@gmail.com>
2023-09-04 19:17:33 -0300

    feat: add play command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,28 +1,50 @@
-import { isMessageInstance } from '@sapphire/discord.js-utilities';
+import { createAudioPlayer, createAudioResource, joinVoiceChannel } from '@discordjs/voice';
 import { Command } from '@sapphire/framework';
-import { ApplicationCommandType, Message } from 'discord.js';
+import { ApplicationCommandType } from 'discord.js';
+import ytdl from 'ytdl-core';
 
-export class BasicCommand extends Command {
+export class PlayCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
     super(context, { ...options });
   }
 
-  public async messageRun(message: Message) {
-    
-  }
-
   public override registerApplicationCommands(registry: Command.Registry) {
     registry.registerChatInputCommand((builder) =>
-      builder.setName('Play').setDescription('Play music')
+      builder.setName('play').setDescription('Play music').addStringOption((opt) => opt.setName("url").setDescription("uma url do youtube").setRequired(true)
+      )
     )
       .registerContextMenuCommand((builder) =>
-      builder
-        .setName('Play')
-        .setType(ApplicationCommandType.Message)
-    );
+        builder
+          .setName('play')
+          .setType(ApplicationCommandType.Message)
+      );
   }
 
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-    
+    const guildId = interaction.guildId!;
+    const guild = interaction.guild;
+    const url = interaction.options.get('url')?.value?.toString() || '';
+
+    const channel = guild?.members.cache.get(interaction.user.id)?.voice.channel;
+
+    if (!channel) {
+      return interaction.reply("tu precisa estar em um canal de voz, tio")
+    }
+
+    const connection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: guildId,
+      adapterCreator: guild.voiceAdapterCreator
+    })
+
+
+    const resource = createAudioResource(ytdl(url, { filter: 'audioonly' }))
+    const player = createAudioPlayer()
+
+    interaction.channel?.send(url)
+    const sub = connection.subscribe(player)
+    player.play(resource)
+    return interaction.reply("Vamo começar essa festa!")
+
   }
 }
