9a4f635135a64697da499ab1172f4a1f7e6902bf
Jovem-Blood <thiagopeixot13@gmail.com>
2023-11-20 17:30:18 -0300

    fix: use deferReply on slow calls

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -5,94 +5,95 @@ import play from 'play-dl';
 import { paused, playing, counter, queue } from '../utils/collections';
 
 export class PlayCommand extends Command {
-  public constructor(context: Command.Context, options: Command.Options) {
-    super(context, { ...options });
-  }
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, { ...options });
+	}
 
-  public override registerApplicationCommands(registry: Command.Registry) {
-    registry.registerChatInputCommand((builder) =>
-      builder.setName('play').setDescription('Play music').addStringOption((opt) => opt.setName("url").setDescription("uma url do youtube").setRequired(true)
-      )
-    )
-      .registerContextMenuCommand((builder) =>
-        builder
-          .setName('play')
-          .setType(ApplicationCommandType.Message)
-      );
-  }
-  public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-    const guildId = interaction.guildId!;
-    const guild = interaction.guild;
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder.setName('play').setDescription('Play music').addStringOption((opt) => opt.setName("url").setDescription("uma url do youtube").setRequired(true)
+			)
+		)
+			.registerContextMenuCommand((builder) =>
+				builder
+					.setName('play')
+					.setType(ApplicationCommandType.Message)
+			);
+	}
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const guildId = interaction.guildId!;
+		const guild = interaction.guild;
+		interaction.deferReply()
 
-    let url = interaction.options.get('url')?.value?.toString() || '';
+		let url = interaction.options.get('url')?.value?.toString() || '';
 
-    const validation = await play.validate(url)
-    if (!validation) {
-      return interaction.reply("Não conheço essa daí não tio")
-    } else if (validation == 'search') {
-      const results = await play.search(url)
-      url = results[0].url
-    }
+		const validation = await play.validate(url)
+		if (!validation) {
+			return interaction.editReply("Não conheço essa daí não tio")
+		} else if (validation == 'search') {
+			const results = await play.search(url)
+			url = results[0].url
+		}
 
-    const channel = guild?.members.cache.get(interaction.user.id)?.voice.channel;
+		const channel = guild?.members.cache.get(interaction.user.id)?.voice.channel;
 
-    if (!channel) {
-      return interaction.reply("tu precisa estar em um canal de voz, tio")
-    }
+		if (!channel) {
+			return interaction.editReply("tu precisa estar em um canal de voz, tio")
+		}
 
-    if (playing.get(guildId) || paused.get(guildId)) {
-      if (queue.has(guildId)) {
-        let musics = queue.get(guildId)!
-        musics?.push(url)
-        queue.set(guildId, musics)
-      } else {
-        queue.set(guildId, [url])
-      }
-      return interaction.reply("Adicionando música a lista")
-    }
+		if (playing.get(guildId) || paused.get(guildId)) {
+			if (queue.has(guildId)) {
+				let musics = queue.get(guildId)!
+				musics?.push(url)
+				queue.set(guildId, musics)
+			} else {
+				queue.set(guildId, [url])
+			}
+			return interaction.editReply("Adicionando música a lista")
+		}
 
-    const connection = joinVoiceChannel({
-      channelId: channel.id,
-      guildId: guildId,
-      adapterCreator: guild.voiceAdapterCreator
-    })
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: guildId,
+			adapterCreator: guild.voiceAdapterCreator
+		})
 
-    const source = await play.stream(url)
+		const source = await play.stream(url)
 
-    const resource = createAudioResource(source.stream, {
-      inputType: source.type
-    })
+		const resource = createAudioResource(source.stream, {
+			inputType: source.type
+		})
 
-    const player = createAudioPlayer()
-    playing.set(guildId, player)
-    queue.set(guildId, [url])
-    counter.set(guildId, 0)
+		const player = createAudioPlayer()
+		playing.set(guildId, player)
+		queue.set(guildId, [url])
+		counter.set(guildId, 0)
 
 
-    connection.subscribe(player)
-    player.play(resource)
-		interaction.reply(`Vamos nessa`)
-    player.on(AudioPlayerStatus.Idle, async () => {
-      let position = counter.get(guildId)! + 1
-      const nextSong = queue.get(guildId)![position]
-      if (!nextSong) {
-        interaction.channel?.send("bye")
-        connection.destroy()
-        queue.delete(guildId)
-        playing.delete(guildId)
-        paused.delete(guildId)
-      } else {
-        const nextSource = await play.stream(nextSong)
-        const nextResource = createAudioResource(nextSource.stream, {
-          inputType: source.type
-        })
-        player.play(nextResource)
-        playing.set(guildId, player)
-        play.video_info(nextSong).then(info => {
-          interaction.channel?.send(`Tocando agora **${info.video_details.title}**`)
-        })
-        counter.set(guildId, position)
-      }
-    })
-  }
+		connection.subscribe(player)
+		player.on(AudioPlayerStatus.Idle, async () => {
+			let position = counter.get(guildId)! + 1
+			const nextSong = queue.get(guildId)![position]
+			if (!nextSong) {
+				interaction.channel?.send("bye")
+				connection.destroy()
+				queue.delete(guildId)
+				playing.delete(guildId)
+				paused.delete(guildId)
+			} else {
+				const nextSource = await play.stream(nextSong)
+				const nextResource = createAudioResource(nextSource.stream, {
+					inputType: source.type
+				})
+				player.play(nextResource)
+				playing.set(guildId, player)
+				play.video_info(nextSong).then(info => {
+					interaction.channel?.send(`Tocando agora **${info.video_details.title}**`)
+				})
+				counter.set(guildId, position)
+			}
+		})
+		player.play(resource)
+		return interaction.editReply("Vamos começar :notes:")
+	}
 }
